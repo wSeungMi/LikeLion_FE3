d18e62b4235af7d431443653884ebd79bee6db4b
wSeungMi <seungmi.dev@gmail.com>
2022-11-13 11:04:31 +0900

    Class: 1112-js초초초기초반 특강(개념 추가)

--- special-lecture/1112_초초초기초반/클로저.js
+++ special-lecture/1112_초초초기초반/클로저.js
@@ -7,15 +7,18 @@ const myFunc = function () {
         let b = 5;
         let c = 6;
 
+        // 스코프 체닝을 통해 외부 함수의 a 값을 참조한다
         a = a + b + c;
         console.log(a);
     };
 
+    // myFunc2(); 함수를 실행하지 않고 리턴하여 밖으로 뺀다
     return myFunc2; // myFunc 함수가 끝날 때 myFunc2 함수를 전달
     // myFunc2()를 반환하면 myFunc2()를 실행한 결과값을 반환한다
     // myFunc2를 반환하면 myFunc2 함수 자체를 반환한다
 };
 
+// return 키워드를 만남으로서 myFunc는 메모리에서 지워진다. 그렇다면 이때 myFunc2에서 참조하고 있던 a는 어떻게 될까?
 // 코드만 봤을 때 innerFunc 안에는 myFunc2()가 들어있는데
 // return을 만나면 함수가 종료됨
 // 그때 myFunc()는 사라지고 myFunc2만 살아남게 되는데 그렇게되면
@@ -32,3 +35,5 @@ innerFunc();
  * 이때 myFunc 함수는 return 키워드를 만나서 종료되고 안에 있는 프로퍼티들은 삭제된다.
  *
  */
+
+// 자바스크립트에서 메모리 관리는 참조 카운팅이라는 방법으로 이루어 진다. a를 여전히 myFunc3에서 참조하고 있기 때문에 myFunc2의 b는 사라지지만 a는 여전히 남아있다.
--- special-lecture/1112_초초초기초반/클로저사용.js
+++ special-lecture/1112_초초초기초반/클로저사용.js
@@ -1,10 +1,13 @@
+// 클로져 공간을 만드는 함수
 function makeClosure() {
+    // 여기 선언된 변수들이 클로져(폐쇠된) 공간에 있게 됨
     const val1 = 100;
     const val2 = 200;
     const val3 = function () {
         console.log("함수");
     }; // 함수도 클로저 방식(비공개)으로 관리하고싶다면!
 
+    // 리턴 키워드를 통해 함수의 변수(val1, val2)를 밖에서 사용할 수 있도록 함수에 담아 반환함
     return {
         getVal1: function () {
             return val1;
@@ -18,6 +21,7 @@ function makeClosure() {
     };
 }
 
+// 이제 result 변수 안에는 makeClosure 함수안에 있던 val1, val2 값이 참조되어있게 된ㅏ. 그리고 여기에 접근하는 방법은 오직 getVal1, getVal2 함수만 가능해진다.
 let result = makeClosure(); // result는 객체
 // val1, val2는 비공개 프로퍼티가 됨
 // 접근하려면
